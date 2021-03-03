e46a5ddde887f3e348692112ee0f3f6fb8304a84
Wesley <wes.beck21@outlook.com>
2021-03-03 10:04:59 -0800

    commit tree 5549ea15d2c6de7c9b88ab807b6e6b84b43c5eee

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    return `${this.name} says meow!`
+  }
+}
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    return `${this.name} says woof!`
+  }
+}
+
+class Bird {
+  constructor(name, gender) {
+    this.name = name;
+    this.gender = gender
+  }
+  speak() {
+    if (this.gender !== 'male') {return `${this.name} says squawk!`}
+    else {
+      return `It's me! ${this.name}, the parrot!`
+    }
+  }
+
+}
